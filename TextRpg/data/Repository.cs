7b1059eb79538e5b52460d07105abd19ea0d4613
김대열 <ajduf7180@gmail.com>
2023-08-22 14:32:02 +0900

    [Update] GetInventoryItems(-대열) 인벤토리를 이제 player 객체에서 가져오도록 수정

--- TextRpg/data/Repository.cs
+++ TextRpg/data/Repository.cs
@@ -16,28 +16,10 @@ public class Repository
 
     private Repository()
     {
-        //todo repository 에서 ... 
     }
 
     public List<IItem> GetInventoryItems(int itemRows, int itemCols)
     {
-        // todo database 또는 파일시스템 연결 이후 데이터소스에서 가져오도록 수정.
-        var items = new List<IItem>(itemRows * itemCols);
-        for (int row = 0; row < itemRows; row++)
-        {
-            for (int col = 0; col < itemCols; col++)
-            {
-                if (row == 3 && col == 0)
-                {
-                    items.Add(new Guinsoo());
-                }
-                else
-                {
-                    items.Add(IItem.Empty);
-                }
-            }
-        }
-
-        return items;
+        return player.Inventory;
     }
 }
